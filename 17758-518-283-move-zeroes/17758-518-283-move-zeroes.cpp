6abd48b73387298ac35bae7e58caf13a46668ad8
faujul <mohammadfaujul@gmail.com>
2025-04-17 22:25:27 +0600

    Time: 11 ms, Memory: 24 MB - LeetHub

--- 17758-518-283-move-zeroes/17758-518-283-move-zeroes.cpp
+++ 17758-518-283-move-zeroes/17758-518-283-move-zeroes.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        long long n=0;
-        for(int i=0; i<nums.size(); i++)
+        long long n=nums.size();
+        int i=0;
+        while(i<n)
         {
-            if(nums[i]!=0)
+            if(nums[i]==0)
             {
-                swap(nums[i],nums[n]);
-                n++;
+               nums.erase(nums.begin()+i);
+                nums.push_back(0);
+                n--;
             }
-
+            else
+            {
+                i++;
+            }
         }
     }
 };
